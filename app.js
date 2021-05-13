307a9439fce2f777f368f2bdaec9ae4df2a5e8d3
Zainab Adegoke <zainabolaitan5@gmail.com>
2021-05-13 07:41:19 +0100

    added auth route and controller files and added cors to routes

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const firebase = require("firebase");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const firebaseConfig = require("./config/config")
 
 var app = express();
-
+firebase.initializeApp(firebaseConfig);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+require('./routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
